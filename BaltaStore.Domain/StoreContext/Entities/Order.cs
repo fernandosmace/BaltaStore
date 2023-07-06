4039bb37df68768dd6300bfd0c20e65119625369
Fernando Macedo <fernando.soares.macedo@hotmail.com>
2023-07-06 00:17:18 -0300

    Criada a modelagem inicial do Order e OrderItem.

---
+++ BaltaStore.Domain/StoreContext/Entities/Order.cs
@@ -0,0 +1,43 @@
+﻿using BaltaStore.Domain.StoreContext.Enums;
+
+namespace BaltaStore.Domain.StoreContext.Entities
+{
+    public class Order
+    {
+        public Customer Customer { get; private set; }
+        public string Number { get; private set; }
+        public DateTime CreateDate { get; private set; }
+        public EOrderStatus Status { get; private set; }
+        public IReadOnlyCollection<OrderItem> Items => _items.ToArray();
+        private readonly IList<OrderItem> _items = new List<OrderItem>();
+        public IReadOnlyCollection<Delivery> Deliveries => _deliveries.ToArray();
+        private readonly IList<Delivery> _deliveries = new List<Delivery>();
+
+        public Order(Customer customer)
+        {
+            Customer = customer;
+            Number = Guid.NewGuid().ToString().Replace("-", "").Substring(0, 8).ToUpper();
+            CreateDate = DateTime.Now;
+            Status = EOrderStatus.Created;
+        }
+
+        public void AddItem(OrderItem item)
+        {
+            //Valida o Item
+            //Adiciona ao Pedido
+            _items.Add(item);
+        }
+
+        public void AddDelivery(Delivery delivery)
+        {
+            //Valida o Delivery
+            //Adiciona ao Pedido
+            _deliveries.Add(delivery);
+        }
+
+        public void Place()
+        {
+
+        }
+    }
+}
---
+++ BaltaStore.Domain/StoreContext/Entities/OrderItem.cs
@@ -0,0 +1,16 @@
+﻿namespace BaltaStore.Domain.StoreContext.Entities
+{
+    public class OrderItem
+    {
+        public Product Product { get; private set; }
+        public decimal Quantity { get; private set; }
+        public decimal Price { get; private set; }
+
+        public OrderItem(Product product, decimal quantity)
+        {
+            Product = product;
+            Quantity = quantity;
+            Price = Product.Price;
+        }
+    }
+}
