a5b09f70da2ac4da0a2bfc2e43f53fb241cfd888
Fernando Macedo <fernando.soares.macedo@hotmail.com>
2023-07-06 00:17:06 -0300

    Criada a modelagem inicial do Delivery.

---
+++ BaltaStore.Domain/StoreContext/Entities/Delivery.cs
@@ -0,0 +1,18 @@
+﻿using BaltaStore.Domain.StoreContext.Enums;
+
+namespace BaltaStore.Domain.StoreContext.Entities
+{
+    public class Delivery
+    {
+        public DateTime CreateDate { get; private set; }
+        public DateTime EstimatedDeliveryDate { get; private set; }
+        public EDeliveryStatus Status { get; private set; }
+
+        public Delivery(DateTime estimatedDeliveryDate)
+        {
+            CreateDate = DateTime.Now;
+            EstimatedDeliveryDate = estimatedDeliveryDate;
+            Status = EDeliveryStatus.Waiting;
+        }
+    }
+}
