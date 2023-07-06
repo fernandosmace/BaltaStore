3dbde37e483972574a7d004bf63c2ad1fe36b53b
Fernando Macedo <fernando.soares.macedo@hotmail.com>
2023-07-06 00:16:26 -0300

    Criados os Enums para tipos de endereço, status de entrega e status de pedido.

---
+++ BaltaStore.Domain/StoreContext/Enums/EOrderStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace BaltaStore.Domain.StoreContext.Enums
+{
+    public enum EOrderStatus
+    {
+        Created = 1,
+        Paid = 2,
+        Shipped = 3,
+        Done = 4,
+        Canceled = 5
+    }
+}
