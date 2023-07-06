586826b43712a56aa9fdfad6663752c83908a4eb
Fernando Macedo <fernando.soares.macedo@hotmail.com>
2023-07-06 00:17:26 -0300

    Criada a modelagem inicial do Product.

---
+++ BaltaStore.Domain/StoreContext/Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿namespace BaltaStore.Domain.StoreContext.Entities
+{
+    public class Product
+    {
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public string Image { get; private set; }
+        public decimal Price { get; private set; }
+        public int QuantityOnHand { get; private set; }
+
+        public Product(string title, string description, string image, decimal price, int quantityOnHand)
+        {
+            Title = title;
+            Description = description;
+            Image = image;
+            Price = price;
+            QuantityOnHand = quantityOnHand;
+        }
+    }
+}
